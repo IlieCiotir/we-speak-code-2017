822da8f4d16555af093109c4b092fc448d5cdc09
Ilie Ciotir(Toskv) <dipixelhunter@gmail.com>
2017-07-05 21:21:54 +0300

    hide show element functions

---
+++ js/hideshow.js
@@ -0,0 +1,9 @@
+function showElement(id) {
+    var element = document.getElementById(id);
+    element.style.display = "block";
+}
+
+function hideElement(id) {
+    var element = document.getElementById(id);
+    element.style.display = "none";
+}
